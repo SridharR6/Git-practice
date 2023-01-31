6f45c9d139672c40a584944c5707c1821969ded1
SridharR6 <98104155+SridharR6@users.noreply.github.com>
2023-01-31 15:14:39 +0530

    Delete Hello.java

--- Hello.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class Hello{
-	public static void main(String args[]){
-		Scanner s = new Scanner(System.in);
-		String name = s.next();
-		System.out.println("Hello "+name);
-	}
-}
