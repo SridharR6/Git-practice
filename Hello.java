9732f1a8a4b9a6a8d0443cec3c7431fd538496f9
siddarths1 <91017970+siddarths1@users.noreply.github.com>
2023-01-31 14:43:08 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,6 +3,6 @@ public class Hello{
 	public static void main(String args[]){
 		Scanner s = new Scanner(System.in);
 		String name = s.next();
-		System.out.println("Hello World");
+		System.out.println("Hello "+name);
 	}
 }
