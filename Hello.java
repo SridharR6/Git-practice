818b6baac9f768e993cd0307c337c426b55c0c3f
SridharR6 <98104155+SridharR6@users.noreply.github.com>
2023-01-31 14:41:59 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class Hello{
 	public static void main(String args[]){
 		Scanner s = new Scanner(System.in);
+		String name = s.next();
 		System.out.println("Hello World");
 	}
 }
